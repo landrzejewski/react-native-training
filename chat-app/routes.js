f0e2ccc140d875e32fd1f554a784e7503bb47a83
landrzejewski <landrzejewski.poczta@gmail.com>
2020-03-31 08:39:35 +0200

    Options

--- chat-app/routes.js
+++ chat-app/routes.js
@@ -5,6 +5,7 @@ import { createStackNavigator } from "react-navigation-stack";
 import Contacts from "./screens/Contacts";
 import Profile from "./screens/Profile";
 import Favorites from "./screens/Favorites";
+import User from "./screens/User";
 import { MaterialIcons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import colors from "./utils/colors";
@@ -27,37 +28,49 @@ const ContactsScreen = createStackNavigator(
 );
 
 const FavoritesScreen = createStackNavigator(
-    {
-      Favorites,
-      Profile
-    },
-    {
-      initialRouteName: "Favorites",
-      navigationOptions: {
-        tabBarIcon: getIcon("star")
-      }
+  {
+    Favorites,
+    Profile
+  },
+  {
+    initialRouteName: "Favorites",
+    navigationOptions: {
+      tabBarIcon: getIcon("star")
     }
-  );
-
-const TabNavigator = createBottomTabNavigator(
-    {
-        Contacts: ContactsScreen,
-        Favorites, FavoritesScreen
-    }, 
-    {
-        initialRouteName: 'Contacts',
-        tabBarPosition: "bottom",
-        tabBarOptions: {
-            style: {
-                backgroundColor: colors.greyLight
-            },
-            showLabel: false,
-            showIcon: true,
-            activeTintColor: colors.blue,
-            inactiveTintColor: colors.greyDark
-        }
-    }
+  }
 );
 
+const UserScreens = createStackNavigator(
+  {
+    User
+  },
+  {
+    initialRouteName: "User",
+    navigationOptions: {
+      tabBarIcon: getIcon("person")
+    }
+  }
+);
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Contacts: ContactsScreen,
+    Favorites:FavoritesScreen,
+    User: UserScreens
+  },
+  {
+    initialRouteName: "Contacts",
+    tabBarPosition: "bottom",
+    tabBarOptions: {
+      style: {
+        backgroundColor: colors.greyLight
+      },
+      showLabel: false,
+      showIcon: true,
+      activeTintColor: colors.blue,
+      inactiveTintColor: colors.greyDark
+    }
+  }
+);
 
 export default createAppContainer(TabNavigator);
